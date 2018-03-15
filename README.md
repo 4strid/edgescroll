08deb57b73593bcc39a174c1afd90f4204fc574f
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2018-03-15 01:46:13 -0700

    add minified versions, auto version, and README

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# edgescroll
-Edge scrolling for web pages
+# edge-scroll
+Configurable edge scrolling for web pages.
+
+Scrolls the page when the cursor goes near the edges of the window.
+
+How to use
+----------
+### Include in script tag
+Include `edge-scroll.js` or `edge-scroll.min.js` on your web page in a `<script>` tag, then in
+your main JavaScript file, call `EdgeScroll` passing the radius from the edges to start scrolling
+at and the maximum scrolling speed (in pixels per second).
+
+```javascript
+var edgeScroll = new EdgeScroll(100, 800);
+```
+
+### Include self executing script tag
+Include `edge-scroll-auto.js` or `edge-scroll-auto.min.js` in a script tag on your web page
+to use default values of 100 pixel radius and 750 pixel per second speed.
+
+No further steps are necessary.
+
+### Include as part of a module loader
+
+Install:
+```
+npm install edge-scroll
+```
+
+In main JS file:
+```
+import EdgeScroll from 'edge-scroll'
+
+const edgeScroll = new EdgeScroll(120, 750)
+```
+
+Scroll Events
+-------------
+The EdgeScroll object has some methods you may find useful
+
+- `onScroll(Function handler)` adds an event listener to listen for scroll events
+- `offScroll(Function handler)` removes a previously added event listener
+
+The handlers are called with the following arguments:
+- Number scrollX : the X scroll position of the window
+- Number scrollY : the Y scroll position of the window
+- Number innerWidth : the width of the window
+- Number innerHeight : the height of the window
+
+```javascript
+var edgeScroll = new EdgeScroll(80, 750);
+
+edgeScroll.onScroll(function (scrollX, scrollY, innerWidth, innerHeight) {
+	console.log(scrollX);
+	console.log(scrollY);
+	console.log(innerWidth);
+	console.log(innerHeight);
+});
+```
+
+The reason to use EdgeScroll handlers rather than just using `addEventListener('scroll')` is
+to prevent layout thrashing: all those properties are cached on the EdgeScroll object once per
+animation frame so the browser doesn't have to do a reflow to access them.
+
+You can access them at any time, but the handlers give them to you every time the page scrolls.
+```
+var edgeScroll = new EdgeScroll(100, 750);
+
+console.log(edgeScroll.scrollX);
+console.log(edgeScroll.scrollY);
+console.log(edgeScroll.innerWidth);
+console.log(edgeScroll.innerHeight);
+```
+
+Dead Zones
+----------
+You can specify deadzones where scrolling should not occur. If you have something in the corner
+that you'd like to be clickable, without causing the window to scroll while hovering over it
+for example.
+
+- `addDeadzone(Element elem)` Adds mouseenter and mouseleave event listeners to prevent scrolling
+on the supplied HTML element.
+- `removeDeadzone(Element elem)` Removes a previously added deadzone
+
+```javascript
+var edgeScroll = new EdgeScroll(100, 750);
+
+edgeScroll.addDeadZone(document.getElementById('corner-widget'))
+```
---
+++ edge-scroll.js
@@ -0,0 +1,151 @@
+(function () {
+
+	function EdgeScroll (radius, speed) {
+		this.radius = radius //distance from the edge to start scrolling
+		this.speed = speed // pixels per second
+		this.scrolling = false
+		this.proximity = null
+		this.clientX = null
+		this.clientY = null
+		this.scrollX = window.scrollX
+		this.scrollY = window.scrollY
+		this.innerWidth = window.innerWidth
+		this.innerHeight = window.innerHeight
+		this.handlers = []
+		this.deadzones = []
+		
+		this.clampedVelocity = function () {
+			var N = 6
+			var p = this.proximity / this.radius
+			return Math.pow(p * -1/2 + 1, N)
+		}
+
+		this.scrollTick = (function (time) {
+			var elapsed = this.lastTime ? time - this.lastTime : 0
+			this.lastTime = time
+			
+			if (this.scrolling === false) {
+				return this.lastTime = 0
+			}
+
+			var centerX = this.innerWidth / 2
+			var centerY = this.innerHeight / 2
+
+			var headingX = this.clientX - centerX
+			var headingY = this.clientY - centerY
+
+			var magnitude = Math.sqrt(Math.pow(headingX, 2) + Math.pow(headingY, 2))
+			var normalizedX = headingX / magnitude
+			var normalizedY = headingY / magnitude
+
+			var velocityX = normalizedX * this.speed * this.clampedVelocity()
+			var velocityY = normalizedY * this.speed * this.clampedVelocity()
+			window.scrollBy(Math.round(velocityX * elapsed / 1000), Math.round(velocityY * elapsed / 1000))
+			this.scrollX = window.scrollX
+			this.scrollY = window.scrollY
+			this.innerWidth = window.innerWidth
+			this.innerHeight = window.innerHeight
+
+			window.requestAnimationFrame(this.scrollTick)
+		}).bind(this)
+
+		this.scroll = function (x, y) {
+			window.scroll(x, y)
+			this.scrollX = window.scrollX
+			this.scrollY = window.scrollY
+			this.innerWidth = window.innerWidth
+			this.innerHeight = window.innerHeight
+		}
+
+		this.onScroll = function (handler) {
+			this.handlers.push(handler)
+		}
+
+		this.offScroll = function (handler) {
+			var i = this.handlers.indexOf(handler)
+			if (i > -1) {
+				this.handlers.splice(i, 1)
+			}
+		}
+
+		var scroll = this
+
+		document.addEventListener('mouseout', function (evt) {
+			if (evt.relatedTarget === null) {
+				scroll.scrolling = false
+			}
+		})
+
+		document.addEventListener('mousemove', function (evt) {
+			scroll.clientX = evt.clientX
+			scroll.clientY = evt.clientY
+			var px1 = evt.clientX - 0
+			var px2 = scroll.innerWidth - evt.clientX
+			var px3 = evt.clientY - 0
+			var px4 = scroll.innerHeight - evt.clientY
+			scroll.proximity = Math.min(px1, px2, px3, px4)
+			if (scroll.proximity < scroll.radius) {
+				if (scroll.dead) {
+					return
+				}
+				if (!scroll.scrolling) {
+					scroll.scrolling = true
+					window.requestAnimationFrame(scroll.scrollTick)
+				}
+			} else {
+				scroll.scrolling = false
+			}
+		})
+
+		document.addEventListener('scroll', function (evt) {
+			var src
+			if (scroll.scrolling) {
+				src = scroll
+			} else {
+				src = window
+			}
+			var scrollX = src.scrollX
+			var scrollY = src.scrollY
+			var innerWidth = src.innerWidth
+			var innerHeight = src.innerHeight
+			scroll.handlers.forEach(function (handler) {
+				handler(scrollX, scrollY, innerWidth, innerHeight)
+			})
+		})
+
+		this.addDeadzone = function (elem) {
+			function enterDeadzone () {
+				scroll.dead = true
+				scroll.scrolling = false
+			}
+
+			function leaveDeadzone () {
+				scroll.dead = false
+			}
+
+			scroll.deadzones.push({
+				elem: elem,
+				enter: enterDeadzone,
+				leave: leaveDeadzone
+			})
+
+			elem.addEventListener('mouseenter', enterDeadzone)
+			elem.addEventListener('mouseleave', leaveDeadzone)
+		}
+
+		this.removeDeadzone = function (elem) {
+			this.deadzones.forEach(function (deadzone) {
+				if (deadzone.elem === elem) {
+					elem.removeEventListener('mouseenter', deadzone.enter)
+					elem.removeEventListener('mouseleave', deadzone.leave)
+				}
+			})
+		}
+	}
+
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = EdgeScroll
+	} else {
+		window.EdgeScroll = EdgeScroll
+	}
+})()
